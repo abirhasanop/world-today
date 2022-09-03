a82488265d0c6cc4b61be2b1412af1edec613e7c
abirhasan <abir77077@gmail.com>
2022-09-03 15:53:43 +0600

    news edited

--- js/index.js
+++ js/index.js
@@ -28,25 +28,40 @@ const loadNews = (categoryId) => {
 const displayNews = (allNews) => {
   // console.log(allNews);
   const allNewsContainer = document.getElementById("news-container");
+  allNewsContainer.textContent = "";
   allNews.forEach((news) => {
     console.log(news);
     const newsDiv = document.createElement("div");
     newsDiv.innerHTML = `
     <div class="card mb-3">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${news.title}</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
                                 content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-      </div>
-    </div>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <img  style="width: 50px;" class="img-fluid rounded-circle" src="${news.author.img}" alt="">
+                                   <h6>${news.author.name}</h6>
+                                </div>
+                                <h5>${news.total_view} <i class="fa-sharp fa-solid fa-eye"></i></h5>
+                                <div>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div><i class="fa-sharp fa-solid fa-arrow-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     `;
     allNewsContainer.appendChild(newsDiv);
   });
