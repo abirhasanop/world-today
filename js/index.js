0480b0d1e095688ac8440d6fd2fbc24683fc9814
abirhasan <abir77077@gmail.com>
2022-09-04 00:46:10 +0600

    no result added

--- js/index.js
+++ js/index.js
@@ -29,6 +29,14 @@ const loadNews = (categoryId) => {
 
 const displayNews = (allNews) => {
   // console.log(allNews);
+  const noResultMsg = document.getElementById("no-result");
+  noResultMsg.textContent = "";
+  if (allNews.length === 0) {
+    noResultMsg.innerHTML = `  
+    <h2 class="text-center text-warning">No Result Found</h2>
+  `;
+  }
+
   const allNewsContainer = document.getElementById("news-container");
   allNewsContainer.textContent = "";
   allNews.forEach((news) => {
@@ -38,21 +46,39 @@ const displayNews = (allNews) => {
     <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+                        <img src="${
+                          news.image_url
+                        }" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${news.title}</h5>
-                            <p class="card-text">${news.details}</p>
+                            <p class="card-text">${
+                              news.details.length > 200
+                                ? news.details.slice(0, 200) + "..."
+                                : news.details
+                            }</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="d-flex align-items-center">
-                                    <img  style="width: 50px;" class="img-fluid rounded-circle" src="${news.author.img}" alt="">
+                                    <img  style="width: 50px;" class="img-fluid rounded-circle" src="${
+                                      news.author.img
+                                    }" alt="">
                                    <div class="d-flex flex-column justify-content-center mt-3 ms-2">
-                                   <h6>${news.author.name}</h6>
+                                   <h6>${
+                                     news.author.name === null ||
+                                     news.author.name === "system"
+                                       ? "Not Found"
+                                       : news.author.name
+                                   }</h6>
                                    <p>10/5/2022</p>
                                    </div>
                                 </div>
-                                <h5>${news.total_view} <i class="fa-sharp fa-solid fa-eye"></i></h5>
+                                <h5>${
+                                  news.total_view === null ||
+                                  news.total_view === "system"
+                                    ? "Not Found"
+                                    : news.total_view
+                                }<i class="fa-sharp fa-solid fa-eye"></i></h5>
                                 <div>
                                     <i class="fa-solid fa-star"></i>
                                     <i class="fa-solid fa-star"></i>
@@ -60,7 +86,9 @@ const displayNews = (allNews) => {
                                     <i class="fa-solid fa-star"></i>
                                     <i class="fa-regular fa-star"></i>
                                 </div>
-                                <a onclick="loadModal('${news._id}')" class="text-primary" data-bs-toggle="modal" data-bs-target="#newsModal"><i class="fa-sharp fa-solid fa-arrow-right"></i></a>
+                                <a onclick="loadModal('${
+                                  news._id
+                                }')" class="text-primary fs-3" data-bs-toggle="modal" data-bs-target="#newsModal"><i class="fa-sharp fa-solid fa-arrow-right"></i></a>
                             </div>
                         </div>
                     </div>
@@ -79,15 +107,24 @@ const loadModal = (newsId) => {
 };
 
 const displayModal = (news) => {
-  console.log(news);
+  // console.log(news);
   const modalTitle = document.getElementById("newsModalLabel");
   modalTitle.innerText = news.title;
 
   const modalBody = document.getElementById("modal-body");
   modalBody.innerHTML = `
-  <h6>Author: ${news.author.name}</h6>
+  <img class="img-fluid my-2" src="${news.thumbnail_url}">
+  <h6>Author: ${
+    news.author.name === null || news.author.name === "system"
+      ? "not found"
+      : news.author.name
+  }</h6>
   <p>Published: ${news.author.published_date}</p>
-  <h6>Total Views: ${news.total_view}</h6>
+  <h6>Total Views: ${
+    news.total_view === null || news.total_view === "system"
+      ? "Not Found"
+      : news.total_view
+  }</h6>
   <h6>Rating: ${news.rating.number}</h6>
   <h6>Badge: ${news.rating.badge}</h6>
   `;
